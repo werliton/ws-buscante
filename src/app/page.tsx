d25b03398f2799968d3b430c4104bd901ba73469
Pedro Mello <contato@cicatriz.dev>
2024-02-14 08:24:59 -0300

    projeto base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,83 @@
-import Image from 'next/image'
+'use client';
+import Image from 'next/image';
+import { KeyboardEvent, useContext, useRef, useState } from 'react';
+import searchIcon from '@/assets/icons/search.svg';
+import googleSearchLogo from '@/assets/images/google-search.webp';
+import homeIllustration from '@/assets/images/home-illustration.webp';
+import { BooksContext } from '@/context/books';
+import BooksList from '@/components/books-list';
+import Loading from '@/assets/images/loading.gif';
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [search, setSearch] = useState('');
+	const inputRef = useRef<HTMLInputElement>(null);
+	const context = useContext(BooksContext);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	if (!context) {
+		throw new Error('useContext must be used within a BooksProvider');
+	}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const { results, searchBooks, loading } = context;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === 'Enter') {
+			searchBooks(search);
+		}
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className='w-full mt-[84px] flex-row justify-center items-center self-center'>
+			<div className='flex-row justify-center items-center mx-4'>
+				<h1 className='text-light-blue font-semibold text-2xl text-center'>
+					Que livro você procura?
+				</h1>
+				<div className='flex items-center justify-center'>
+					<div
+						className='flex justify-between absolute sm:w-1/2 md:w-1/2 lg:w-1/3 mt-3 px-6 sm:px-3 cursor-pointer z-10'
+						onClick={() => inputRef.current?.focus()}
+					>
+						<Image
+							src={googleSearchLogo}
+							alt='ícone do google'
+							className={`w-16 h-5  ${search.length !== 0 ? 'opacity-0' : 'opacity-35'}`}
+						/>
+						<Image
+							src={searchIcon}
+							alt='ícone de busca'
+							className='w-5 h-5 z-10'
+							onClick={() => searchBooks(search)}
+						/>
+					</div>
+					<input
+						ref={inputRef}
+						type='text'
+						className='p-2 pl-8 mt-3 border-2 border-light-blue rounded-[10px] sm:w-1/2 md:w-1/2 lg:w-1/3'
+						onChange={(e) => setSearch(e.target.value)}
+						onKeyDown={handleKeyDown}
+					/>
+				</div>
+				<p className='text-dark-gray font-normal text-base text-center mt-2'>
+					Busque por assunto, autoria, nome...
+				</p>
+				{loading && (
+					<div className='flex items-center w-full justify-center mt-4'>
+						<Image src={Loading} alt='carregando...' />
+					</div>
+				)}
+				{!loading && !results.length && (
+					<div className='flex flex-col sm:flex-row justify-between w-full md:px-[80px] lg:px-[100px] xl:px-[200px] 2xl:px-[600px] items-center sm:items-start'>
+						<h2 className='text-dark-blue font-semibold text-4xl text-center mt-8 mr-8'>
+							Busque o livro que quiser na nossa estante!
+						</h2>
+						<Image
+							src={homeIllustration}
+							alt='ilustração da página inicial'
+							className='mt-8 sm:mt-2'
+						/>
+					</div>
+				)}
+			</div>
+			{!loading && results.length && <BooksList books={results} />}
+		</main>
+	);
 }
